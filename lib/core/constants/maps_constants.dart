ab30f1c39211ff782d807d47c301538024cad2c4
sakib-shaikh-github <96862443+sakib-shaikh-github@users.noreply.github.com>
2023-03-14 13:53:08 +0530

    Update maps_constants.dart

--- lib/core/constants/maps_constants.dart
+++ lib/core/constants/maps_constants.dart
@@ -2,10 +2,10 @@ import 'package:mapbox_gl/mapbox_gl.dart';
 
 class MapConstants {
   static const String mapBoxAccessToken =
-      'pk.eyJ1Ijoic2FraWJtYXBzIiwiYSI6ImNsZGtnODhkNDBrZzYzb2xnOXZqMGp2MjQifQ.bsOkN3nPZ0A-EsOGKqpaCQ';
+      'pk';
 
   static const String mapBoxStyleId =
-      'https://api.mapbox.com/styles/v1/sakibmaps/clct3j2ad001714o2wfgjrbyf/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1Ijoic2FraWJtYXBzIiwiYSI6ImNsY3JoY25nYjBmeGozcHF2OHR1enlwNjkifQ.6XLpH7l82lEIV3Uhr4-X_A';
+      'https://api.mapbox.com/styles/v1/sakibmaps/clct3j2ad001714o2wfgjrbyf/tiles/256/{z}/{x}/{y}@2x?access_token=pk';
 
   static LatLng defaultlocation = const LatLng(18.9294, 72.8310);
 
